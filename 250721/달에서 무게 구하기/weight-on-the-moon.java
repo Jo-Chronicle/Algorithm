7b1f799c8baf8539c62c8879ed25ac6ae6395c8b
Jo-Chronicle <jm.trillion36@gmail.com>
2025-07-21 21:53:27 +0900

    Title: 달에서 무게 구하기, Time: 94ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250721/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,20 @@
+// public class Main {
+//     public static void main(String[] args) {
+//         // Please write your code here.
+//         int w = 13;
+//         double e = 0.165;
+//        // double pi = w*e;
+//         System.out.printf("%d * %6f = %.6f",w,e,w*e);
+//     }
+// }
+
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int w = 13;
+        double e = 0.165;
+        double pi = w*e;
+        System.out.printf("%d * %6f = %.6f",w,e,pi);
+    }
+}
