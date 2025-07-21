5f4064a7aaf6d75b1f46a139727794d6da45a016
Jo-Chronicle <jm.trillion36@gmail.com>
2025-07-21 19:14:22 +0900

    Title: 합과 평균, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250721/합과 평균/sum-and-avg.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sum = a+b;
+        double avg = sum / 2.0;
+
+        //System.out.printf("%d %.1f\n",sum, avg);
+        System.out.printf(sum + " %.1f",avg);
+    }
+}
