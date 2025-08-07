a08a7730d1fe6b63a4133efe6cbc05ba8c34674e
Jo-Chronicle <jm.trillion36@gmail.com>
2025-08-07 14:45:26 +0900

    Title: 배열 채우고 출력하기, Time: 87ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250807/배열 채우고 출력하기/filling-array-and-print.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] n = {"L", "E", "B", "R", "O", "S", "C", "O", "D", "E"};
+
+        for(int i=n.length-1; i>=0; i--){
+            System.out.print(n[i]); 
+        }
+}}
