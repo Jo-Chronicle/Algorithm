7b8558ae7bd05fd0091b927fb7e8373904b39654
Jo-Chronicle <jm.trillion36@gmail.com>
2025-08-08 13:48:18 +0900

    Title: 더 긴 문자열, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

--- 250808/더 긴 문자열/longer-string.java
+++ 250808/더 긴 문자열/longer-string.java
@@ -6,7 +6,13 @@ public class Main {
         String a = sc.next();
         String b = sc.next();
 
-        if(a.length > b.len)
+        if(a.length() > b.length()){
+            System.out.print(a+" "+a.length());
+        }else if(a.length() < b.length()){
+            System.out.print(b+" "+b.length());
+        }else{
+            System.out.print("same");
+        }
 
     }
 }
